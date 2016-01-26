d81e619c1db382c94c2a768a2b8c5de447d55c0a
Moshe Brevda <mbrevda@gmail.com>
2016-01-26 23:30:53 +0200

    Add debuging

--- tests/composer-which.sh
+++ tests/composer-which.sh
@@ -3,7 +3,7 @@
 function info() {
     if [ "${1}" = "Composer found at $_COMPOSER_DIR/composer" ]; then
         passTest "${BASH_SOURCE[0]}"
-        . build-esen.sh # only intercept info() once
+        reset # only intercept info() once
     else
         failTest "${BASH_SOURCE[0]} (${1})"
     fi
