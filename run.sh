4c6ccf91acc60d92fa3a2d4770a88501f5d2fb0d
Moshe Brevda <mbrevda@gmail.com>
2016-01-26 23:00:00 +0200

    Always check for php

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+if hash php 2>/dev/null
+then
+	info "php version $(php --version) found"
+	info "from location $(which php)"
+else
+   	fail "PHP not found!"
+fi
 
 source src/composerPath.sh
 source src/setCache.sh
